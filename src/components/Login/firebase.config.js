a9484065b1870f89a2f437ba237ea90efa37392f
Iftekhar Hasan <iftekharhasanrifat@gmail.com>
2021-04-01 17:47:44 +0100

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAZMZydCqcKd1R-bucflkY5ZJzviE_OnzI",
+  authDomain: "burj-al-arab-a7473.firebaseapp.com",
+  projectId: "burj-al-arab-a7473",
+  storageBucket: "burj-al-arab-a7473.appspot.com",
+  messagingSenderId: "191260877828",
+  appId: "1:191260877828:web:94275b8eecc3fa224f8151"
+};
+  export default firebaseConfig;
